3e2b3edc61561082cbf2f2ce2c65d79ffac0052c
AlvinWachiraKE <98809053+Crimson-Hat@users.noreply.github.com>
2023-01-15 04:08:52 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  //config
+  apiKey: "AIzaSyCjFEUJgT9iGncHIvoeCll5DUnLmrl4sTk",
+  authDomain: "deblees.firebaseapp.com",
+  projectId: "deblees",
+  storageBucket: "deblees.appspot.com",
+  messagingSenderId: "309945849585",
+  appId: "1:309945849585:web:18b2144c5d2406c4b1bb50",
+  measurementId: "G-FJXD0JDMTZ"  
 };
 
 const app = initializeApp(firebaseConfig);
